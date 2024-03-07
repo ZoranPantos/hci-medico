0abfd28840fb8353eb640ef394dcb0917061fb14
Zoran Pantos <pantos78zoran@gmail.com>
2024-03-07 13:19:05 +0100

    feat: setup database and ef core

---
+++ src/HciMedico.Library/Data/AppDbContext.cs
@@ -0,0 +1,27 @@
+﻿using HciMedico.Library.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace HciMedico.Library.Data;
+
+public class AppDbContext : DbContext
+{
+    private string connectionString = "Server=localhost;Port=3306;Database=medico;Uid=root;Pwd=root";
+
+    public DbSet<Test> Tests { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Test>().HasData(new Test { Id = 1, TestName = "Test 1" });
+        modelBuilder.Entity<Test>().HasData(new Test { Id = 2, TestName = "Test 2" });
+        modelBuilder.Entity<Test>().HasData(new Test { Id = 3, TestName = "Test 3" });
+    }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseMySql(
+            connectionString,
+            ServerVersion.AutoDetect(connectionString),
+            optionsBuilder => optionsBuilder.MigrationsAssembly(typeof(AppDbContext).Assembly.FullName)
+            );
+    }
+}
---
+++ src/HciMedico.Library/Data/Migrations/20240307120601_TestMigration.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace HciMedico.Library.Migrations
+{
+    /// <inheritdoc />
+    public partial class TestMigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterDatabase()
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateTable(
+                name: "Tests",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    TestName = table.Column<string>(type: "longtext", nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4")
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Tests", x => x.Id);
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.InsertData(
+                table: "Tests",
+                columns: new[] { "Id", "TestName" },
+                values: new object[,]
+                {
+                    { 1, "Test 1" },
+                    { 2, "Test 2" },
+                    { 3, "Test 3" }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Tests");
+        }
+    }
+}
