4e6c5af8f9cfa5d5bb403d8e9c9593e971169cd1
Zoran Pantos <pantos78zoran@gmail.com>
2024-03-11 23:18:35 +0100

    feat: add hashing service

---
+++ src/HciMedico.App/Services/HashingService.cs
@@ -0,0 +1,20 @@
+﻿using System.Text;
+using System.Security.Cryptography;
+
+namespace HciMedico.App.Services;
+
+public class HashingService
+{
+    public static string GetHashString(string password)
+    {
+        byte[] passwordBytes = Encoding.UTF8.GetBytes(password);
+
+        using var sha256 = SHA256.Create();
+
+        byte[] hashBytes = sha256.ComputeHash(passwordBytes);
+
+        string hashedPassword = Convert.ToBase64String(hashBytes);
+
+        return hashedPassword;
+    }
+}
