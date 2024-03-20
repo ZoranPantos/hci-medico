6d08925db2a83af318f9d52d066d585b2f8f12c3
Zoran Pantos <pantos78zoran@gmail.com>
2024-03-20 21:41:37 +0100

    feat: implement initial data access layer

---
+++ src/HciMedico.Library/Data/Repositories/UserAccountRepository.cs
@@ -0,0 +1,10 @@
+﻿using HciMedico.Library.Models;
+
+namespace HciMedico.Library.Data.Repositories;
+
+public class UserAccountRepository : BaseRepository<UserAccount>
+{
+    public UserAccountRepository(AppDbContext context) : base(context)
+    {
+    }
+}
