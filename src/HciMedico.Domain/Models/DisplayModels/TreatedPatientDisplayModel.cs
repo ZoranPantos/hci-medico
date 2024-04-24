8cf4f0c4463048724db7d0a3e61e2ee57b09b2b3
Zoran Pantos <pantos78zoran@gmail.com>
2024-04-24 18:55:54 +0200

    chore: update display model

--- src/HciMedico.Domain/Models/DisplayModels/TreatedPatientDisplayModel.cs
+++ src/HciMedico.Domain/Models/DisplayModels/TreatedPatientDisplayModel.cs
@@ -2,6 +2,7 @@
 
 public sealed class TreatedPatientDisplayModel
 {
+    public int Id { get; set; }
     public string FullName { get; set; } = string.Empty;
     public DateTime DateOfBirth { get; set; } = DateTime.MinValue;
     public int NumberOfVisits { get; set; }
