28f1e8cc1ed13790228cbc356cdb3b94b7377a97
Zoran Pantos <pantos78zoran@gmail.com>
2024-04-28 20:45:07 +0200

    chore: add emtpy schedule view and view model

---
+++ src/HciMedico.App/ViewModels/Shared/ScheduleViewModel.cs
@@ -0,0 +1,11 @@
+﻿using Caliburn.Micro;
+
+namespace HciMedico.App.ViewModels.Shared;
+
+public class ScheduleViewModel : Conductor<object>
+{
+    public ScheduleViewModel()
+    {
+
+    }
+}
---
+++ src/HciMedico.App/Views/Shared/ScheduleView.xaml.cs
@@ -0,0 +1,11 @@
+﻿using System.Windows.Controls;
+
+namespace HciMedico.App.Views.Shared;
+
+public partial class ScheduleView : UserControl
+{
+    public ScheduleView()
+    {
+        InitializeComponent();
+    }
+}
