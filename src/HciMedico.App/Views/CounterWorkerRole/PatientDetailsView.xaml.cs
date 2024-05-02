7f3534949915c776a5b6e1a43f9954869f49293f
Zoran Pantos <pantos78zoran@gmail.com>
2024-05-02 21:52:49 +0200

    feat: add initial skeleton for patient details view

---
+++ src/HciMedico.App/Views/CounterWorkerRole/PatientDetailsView.xaml.cs
@@ -0,0 +1,8 @@
+﻿using System.Windows.Controls;
+
+namespace HciMedico.App.Views.CounterWorkerRole;
+
+public partial class PatientDetailsView : UserControl
+{
+    public PatientDetailsView() => InitializeComponent();
+}
