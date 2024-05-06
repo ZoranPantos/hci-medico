03ae8bd06195fbf0f2baf7a73ebb067c2bb927c7
Zoran Pantos <pantos78zoran@gmail.com>
2024-05-06 14:37:13 +0200

    feat: add background color converter for schedule calendar

---
+++ src/HciMedico.App/Converters/CalendarCellBackgroundConverter.cs
@@ -0,0 +1,28 @@
+﻿using HciMedico.Domain.Models.DisplayModels;
+using System.Globalization;
+using System.Windows.Data;
+using System.Windows.Media;
+
+namespace HciMedico.App.Converters;
+
+public class CalendarCellBackgroundConverter : IValueConverter
+{
+    public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        if (value is ScheduleCellDisplayModel cell)
+        {
+            if (cell.IsToday)
+                return Brushes.LightGreen;
+            
+            if (cell.IsCurrentMonth)
+                return Brushes.LightGray;
+
+            return Brushes.White;
+        }
+
+        return Brushes.White;
+    }
+
+    public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture) =>
+        throw new NotImplementedException();
+}
