ce83ef07e4c5b1f405b0639c58ab1c94d16051a3
Zoran Pantos <pantos78zoran@gmail.com>
2024-05-09 13:57:46 +0200

    feat: implement UI view for editing patient details and base view model

---
+++ src/HciMedico.App/ViewModels/CounterWorkerRole/EditPatientDetailsViewModel.cs
@@ -0,0 +1,148 @@
+﻿using Caliburn.Micro;
+using HciMedico.Domain.Models.Enums;
+
+namespace HciMedico.App.ViewModels.CounterWorkerRole;
+
+public class EditPatientDetailsViewModel : Conductor<object>
+{
+    private string _firstName = string.Empty;
+    public string FirstName
+    {
+        get => _firstName;
+        set
+        {
+            _firstName = value;
+            NotifyOfPropertyChange(() => FirstName);
+        }
+    }
+
+    private string _lastName = string.Empty;
+    public string LastName
+    {
+        get => _lastName;
+        set
+        {
+            _lastName = value;
+            NotifyOfPropertyChange(() => LastName);
+        }
+    }
+
+    private string _uid = string.Empty;
+    public string Uid
+    {
+        get => _uid;
+        set
+        {
+            _uid = value;
+            NotifyOfPropertyChange(() => Uid);
+        }
+    }
+
+    public Gender[] GenderOptions { get; } = Enum.GetValues<Gender>();
+
+    private Gender _selectedGender;
+    public Gender SelectedGender
+    {
+        get => _selectedGender;
+        set
+        {
+            _selectedGender = value;
+            NotifyOfPropertyChange(() => SelectedGender);
+        }
+    }
+
+    private DateTime _dateOfBirth;
+    public DateTime DateOfBirth
+    {
+        get => _dateOfBirth;
+        set
+        {
+            _dateOfBirth = value;
+            NotifyOfPropertyChange(() => DateOfBirth);
+        }
+    }
+
+    private string _country = string.Empty;
+    public string Country
+    {
+        get => _country;
+        set
+        {
+            _country = value;
+            NotifyOfPropertyChange(() => Country);
+        }
+    }
+
+    private string _city = string.Empty;
+    public string City
+    {
+        get => _city;
+        set
+        {
+            _city = value;
+            NotifyOfPropertyChange(() => City);
+        }
+    }
+
+    public string _street = string.Empty;
+    public string Street
+    {
+        get => _street;
+        set
+        {
+            _street = value;
+            NotifyOfPropertyChange(() => Street);
+        }
+    }
+
+    public string _number = string.Empty;
+    public string Number
+    {
+        get => _number;
+        set
+        {
+            _number = value;
+            NotifyOfPropertyChange(() => Number);
+        }
+    }
+
+    public string _email = string.Empty;
+    public string Email
+    {
+        get => _email;
+        set
+        {
+            _email = value;
+            NotifyOfPropertyChange(() => Email);
+        }
+    }
+
+    public string _telephoneNumber = string.Empty;
+    public string TelephoneNumber
+    {
+        get => _telephoneNumber;
+        set
+        {
+            _telephoneNumber = value;
+            NotifyOfPropertyChange(() => TelephoneNumber);
+        }
+    }
+
+    private string _validationMessage = string.Empty;
+    public string ValidationMessage
+    {
+        get => _validationMessage;
+        set
+        {
+            _validationMessage = value;
+            NotifyOfPropertyChange(() => ValidationMessage);
+        }
+    }
+
+    public void Save()
+    {
+
+    }
+
+    public async Task Cancel() => await TryCloseAsync();
+}
---
+++ src/HciMedico.App/Views/CounterWorkerRole/EditPatientDetailsView.xaml.cs
@@ -0,0 +1,8 @@
+﻿using System.Windows;
+
+namespace HciMedico.App.Views.CounterWorkerRole;
+
+public partial class EditPatientDetailsView : Window
+{
+    public EditPatientDetailsView() => InitializeComponent();
+}
