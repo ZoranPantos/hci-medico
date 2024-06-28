c2b320dd9801b0c72d169650c6a64435afe2c149
Zoran Pantos <pantos78zoran@gmail.com>
2024-06-28 22:58:34 +0200

    test: search patients

--- tests/HciMedico.UnitTests/Services/SearchService/SearchServiceTests.cs
+++ tests/HciMedico.UnitTests/Services/SearchService/SearchServiceTests.cs
@@ -1,4 +1,7 @@
-﻿using Xunit.Abstractions;
+﻿using HciMedico.Domain.Models;
+using HciMedico.UnitTests.Services.SearchService.TestData;
+using System.Xml.Linq;
+using Xunit.Abstractions;
 
 namespace HciMedico.UnitTests.Services.SearchService;
 
@@ -11,5 +14,76 @@ public class SearchServiceTests : IClassFixture<SearchServiceFixture>
     {
         _fixture = fixture;
         _output = output;
+    }
+
+    [Theory]
+    [ClassData(typeof(PatientsFirstNameWithResultsTestData))]
+    public void SearchMatchingPatientByFirstNameTest(string firstName, List<Patient> targetSet)
+    {
+        var resultSet = _fixture.SearchService.SearchPatients(targetSet, firstName);
+
+        _output.WriteLine($"Input key: {firstName}");
+        OutputTargetSet(targetSet);
+        OutputSearchResults(1, resultSet);
+
+        Assert.Equal(1, resultSet?.Count);
+        resultSet?.ForEach(result => Assert.Equal(firstName.ToLower(), result.FirstName.ToLower()));
+    }
+
+    [Theory]
+    [ClassData(typeof(PatientsLastNameWithResultsTestData))]
+    public void SearchMatchingPatientByLastNameTest(string lastName, List<Patient> targetSet)
+    {
+        var resultSet = _fixture.SearchService.SearchPatients(targetSet, lastName);
+
+        _output.WriteLine($"Input key: {lastName}");
+        OutputTargetSet(targetSet);
+        OutputSearchResults(2, resultSet);
+
+        Assert.Equal(2, resultSet?.Count);
+        resultSet?.ForEach(result => Assert.Equal(lastName.ToLower(), result.LastName.ToLower()));
+    }
+
+    [Theory]
+    [ClassData(typeof(PatientsFullNameWithResultsTestData))]
+    public void SearchMatchingPatientByFullNameTest(string fullName, List<Patient> targetSet)
+    {
+        var resultSet = _fixture.SearchService.SearchPatients(targetSet, fullName);
+
+        _output.WriteLine($"Input key: {fullName}");
+        OutputTargetSet(targetSet);
+        OutputSearchResults(1, resultSet);
+
+        Assert.Equal(1, resultSet?.Count);
+        resultSet?.ForEach(result => Assert.Equal(fullName.ToLower(), result.FullName.ToLower()));
+    }
+
+    [Theory]
+    [ClassData(typeof(PatientsFullNameWithoutResultsTestData))]
+    public void SearchMatchingPatientByFullNameInNotContainingSet(string fullName, List<Patient> targetSet)
+    {
+        var resultSet = _fixture.SearchService.SearchPatients(targetSet, fullName);
+
+        _output.WriteLine($"Input key: {fullName}");
+        OutputTargetSet(targetSet);
+        OutputSearchResults(0, resultSet);
+
+        Assert.Equal(0, resultSet?.Count);
+    }
+
+    private void OutputTargetSet(List<Patient> targetSet)
+    {
+        _output.WriteLine("Target set:");
+        targetSet.ForEach(patient => _output.WriteLine($"    {patient.FullName}"));
+        _output.WriteLine("-----------------------------------------");
+    }
+
+    private void OutputSearchResults(int expectedCount, List<Patient>? results)
+    {
+        _output.WriteLine($"Expected result count: {expectedCount} | Actual result count: {results?.Count}");
+        _output.WriteLine("Results:");
+
+        results?.ForEach(patient => _output.WriteLine($"    {patient.FullName}"));
+        _output.WriteLine("-----------------------------------------");
     }
 }
---
+++ tests/HciMedico.UnitTests/Services/SearchService/TestData/PatientsFirstNameWithResultsTestData.cs
@@ -0,0 +1,26 @@
+﻿using HciMedico.Domain.Models;
+using System.Collections;
+
+namespace HciMedico.UnitTests.Services.SearchService.TestData;
+
+public class PatientsFirstNameWithResultsTestData : IEnumerable<object[]>
+{
+    public IEnumerator<object[]> GetEnumerator()
+    {
+        yield return new object[]
+        {
+            "Ana",
+            new List<Patient>()
+            {
+                new() { FirstName = "Ana", LastName = "Stanojević" },
+                new() { FirstName = "Boris", LastName = "Borisavljević" },
+                new() { FirstName = "Jovana", LastName = "Jovanović" },
+                new() { FirstName = "Nikola", LastName = "Nikolić" },
+                new() { FirstName = "Milana", LastName = "Milanović" },
+                new() { FirstName = "Stana", LastName = "Stanojević" }
+            }
+        };
+    }
+
+    IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+}
