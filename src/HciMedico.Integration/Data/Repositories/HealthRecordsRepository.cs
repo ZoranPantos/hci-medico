ca14dc145839cfa1a42850d627af59fb955e6c86
Zoran Pantos <pantos78zoran@gmail.com>
2024-09-09 11:28:47 +0200

    feat: add health records repository

---
+++ src/HciMedico.Integration/Data/Repositories/HealthRecordsRepository.cs
@@ -0,0 +1,10 @@
+﻿using HciMedico.Domain.Models;
+
+namespace HciMedico.Integration.Data.Repositories;
+
+public class HealthRecordsRepository : BaseRepository<HealthRecord>
+{
+    public HealthRecordsRepository(AppDbContext context) : base(context)
+    {
+    }
+}
