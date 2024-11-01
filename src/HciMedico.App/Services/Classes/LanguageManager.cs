3218a4c7c5ec59ade55b20c841f0b7a687d865ff
Zoran Pantos <pantos78zoran@gmail.com>
2024-11-01 22:37:26 +0100

    feat: add language manager

---
+++ src/HciMedico.App/Services/Classes/LanguageManager.cs
@@ -0,0 +1,24 @@
+﻿using HciMedico.Domain.Models.Enums;
+using System.Windows;
+
+namespace HciMedico.App.Services.Classes;
+
+public static class LanguageManager
+{
+    public static void SetLanguage(ApplicationLanguage? language = null)
+    {
+        ResourceDictionary languageDictionary = [];
+
+        languageDictionary.Source = language switch
+        {
+            ApplicationLanguage.Serbian => new Uri("Resources/Language/Strings.sr.xaml", UriKind.Relative),
+
+            // English by default
+            _ => new Uri("Resources/Language/Strings.en.xaml", UriKind.Relative),
+        };
+        Application.Current.Resources.MergedDictionaries.Add(languageDictionary);
+
+        if (UserContext.CurrentUser is not null)
+            UserContext.CurrentUser.UserSettings.ApplicationLanguage = language ?? ApplicationLanguage.English;
+    }
+}
